52495a203c4015ff2f743a58d718e4a0bcd1867f
Tate Kaneshige <tatekaneshige@Macintosh-9.local>
2014-01-10 18:28:30 -1000

    pt-construction site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PtConstruction::Application.config.secret_key_base = '715a8896e9495ab18ed8e7d709e233e0a0cda8ff89898d9fefb2b0e9e4d5f9c86c3e955f61f049c59ffb2cc422600f3dc6bfc6a720d8757712a8acf3d3cc983a'
